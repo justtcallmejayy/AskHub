7f5d7e9fc8cdfd1ab86d2644e155c65a2e7eef10
JAY CHOKSI <jaychokxii1804@gmail.com>
2024-11-24 11:27:52 -0500

    Added delete route

--- index.js
+++ index.js
@@ -114,6 +114,14 @@ app.get("/posts/:id/edit", (req, res) => {
   res.render("edit.ejs", { post });
 });
 
+app.delete("/posts/:id", (req,res)=>{
+
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
+
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
