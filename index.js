41072a4ce17f6157fed2ac84a16b56d17fb2b294
JAY CHOKSI <jaychokxii1804@gmail.com>
2024-12-17 10:50:51 -0500

    Reformatted the index file

--- index.js
+++ index.js
@@ -114,12 +114,10 @@ app.get("/posts/:id/edit", (req, res) => {
   res.render("edit.ejs", { post });
 });
 
-app.delete("/posts/:id", (req,res)=>{
-
+app.delete("/posts/:id", (req, res) => {
   let { id } = req.params;
   posts = posts.filter((p) => id !== p.id);
   res.redirect("/posts");
-
 });
 
 app.listen(port, () => {
