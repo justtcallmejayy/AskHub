72cb94d1aeec267644d5f984db901adc5add5a6e
JAY CHOKSI <jaychokxii1804@gmail.com>
2025-01-21 15:08:21 -0500

    Added new ROOT ROUTE

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require("uuid");
 const methodOverride = require("method-override");
 
 // Path to your JSON data file
-const postsPath = path.join(__dirname, "..", "data.json"); // Note '..' because data.json is one level up
+const postsPath = path.join(__dirname, "..", "data.json");
 let posts = JSON.parse(fs.readFileSync(postsPath, "utf8"));
 
 // Middleware
@@ -19,6 +19,13 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "..", "views"));
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+// ---------------------------------------------------
+//  ADD A ROUTE FOR "/" (ROOT) => Redirect to "/posts"
+// ---------------------------------------------------
+app.get("/", (req, res) => {
+  res.redirect("/posts");
+});
+
 // ROUTES
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
@@ -29,30 +36,33 @@ app.get("/posts/new", (req, res) => {
 });
 
 app.post("/posts", (req, res) => {
-  let { username, content } = req.body;
-  let id = uuidv4();
+  const { username, content } = req.body;
+  const id = uuidv4();
   posts.push({ id, username, content, upvotes: 0, downvotes: 0 });
+
   // Write changes to file (won't persist on Vercel, but works locally)
   fs.writeFileSync(postsPath, JSON.stringify(posts, null, 4));
+
   res.redirect("/posts");
 });
 
 app.get("/posts/:id", (req, res) => {
-  let { id } = req.params;
-  let post = posts.find((p) => p.id === id);
+  const { id } = req.params;
+  const post = posts.find((p) => p.id === id);
   res.render("show.ejs", { post });
 });
 
 app.get("/posts/:id/edit", (req, res) => {
-  let { id } = req.params;
-  let post = posts.find((p) => p.id === id);
+  const { id } = req.params;
+  const post = posts.find((p) => p.id === id);
   res.render("edit.ejs", { post });
 });
 
 app.patch("/posts/:id", (req, res) => {
-  let { id } = req.params;
-  let newContent = req.body.content;
-  let post = posts.find((p) => p.id === id);
+  const { id } = req.params;
+  const newContent = req.body.content;
+  const post = posts.find((p) => p.id === id);
+
   if (post) {
     post.content = newContent;
     fs.writeFileSync(postsPath, JSON.stringify(posts, null, 4));
@@ -61,29 +71,33 @@ app.patch("/posts/:id", (req, res) => {
 });
 
 app.delete("/posts/:id", (req, res) => {
-  let { id } = req.params;
+  const { id } = req.params;
   posts = posts.filter((p) => p.id !== id);
   fs.writeFileSync(postsPath, JSON.stringify(posts, null, 4));
   res.redirect("/posts");
 });
 
 app.post("/posts/:id/upvote", (req, res) => {
-  let { id } = req.params;
-  let post = posts.find((p) => p.id === id);
+  const { id } = req.params;
+  const post = posts.find((p) => p.id === id);
+
   if (post) {
     post.upvotes = (post.upvotes || 0) + 1;
     fs.writeFileSync(postsPath, JSON.stringify(posts, null, 4));
   }
+
   res.redirect("/posts");
 });
 
 app.post("/posts/:id/downvote", (req, res) => {
-  let { id } = req.params;
-  let post = posts.find((p) => p.id === id);
+  const { id } = req.params;
+  const post = posts.find((p) => p.id === id);
+
   if (post) {
     post.downvotes = (post.downvotes || 0) + 1;
     fs.writeFileSync(postsPath, JSON.stringify(posts, null, 4));
   }
+
   res.redirect("/posts");
 });
 
